37fbe0ffe7d76d4a482755cd0c82360c66a6faf5
obscuremax <maxxam@yandex.ru>
2020-02-20 22:09:05 +0300

    resolve typical-arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == null || array == false || array == undefined || array.length == undefined) {
+        return 0;
+    }
+   let min
+    for(let i=0; i<array.length; i++) {
+        if (i === 0) {
+            min = array[i]
+        } else if(array[i]<min){
+            min = array[i]
+        }
+    }
+    return min
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == null || array == false || array == undefined || array.length == undefined) {
+        return 0;
+    }
+    let max
+    for(let i=0; i<array.length; i++) {
+        if (i === 0) {
+            max = array[i]
+        } else if(array[i]>max){
+            max = array[i]
+        }
+    }
+    return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == null || array == false || array == undefined || array.length == undefined) {
+        return 0;
+    }
+    let num = array.length
+    let newArr = array.reduce((accum, next) => accum+next)
+    return newArr/num
 }
